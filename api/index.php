fdfbdc371f2550859153801d5ee4b395e6f37eb4
Hanif25-github <70031945+Hanif25-github@users.noreply.github.com>
2026-04-03 16:46:05 +0530

    new directory set for vercel

--- api/index.php
+++ api/index.php
@@ -5,6 +5,23 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$cachePath = sys_get_temp_dir().'/laravel-bootstrap-cache';
+
+if (! is_dir($cachePath)) {
+    mkdir($cachePath, 0777, true);
+}
+
+$_ENV['APP_SERVICES_CACHE'] = $cachePath.'/services.php';
+$_SERVER['APP_SERVICES_CACHE'] = $cachePath.'/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $cachePath.'/packages.php';
+$_SERVER['APP_PACKAGES_CACHE'] = $cachePath.'/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = $cachePath.'/config.php';
+$_SERVER['APP_CONFIG_CACHE'] = $cachePath.'/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $cachePath.'/routes-v7.php';
+$_SERVER['APP_ROUTES_CACHE'] = $cachePath.'/routes-v7.php';
+$_ENV['APP_EVENTS_CACHE'] = $cachePath.'/events.php';
+$_SERVER['APP_EVENTS_CACHE'] = $cachePath.'/events.php';
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
 	require $maintenance;
 }
