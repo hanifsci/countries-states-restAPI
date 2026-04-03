efbb7ac9d653f53654fe60fdab4b8b43c91c0d31
Hanif25-github <70031945+Hanif25-github@users.noreply.github.com>
2026-04-03 16:42:12 +0530

    fix rendering error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -23,6 +25,22 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $throwable): bool {
             return true;
+        });
+
+        $exceptions->render(function (Throwable $throwable, Request $request) {
+            $status = 500;
+
+            if ($throwable instanceof HttpExceptionInterface) {
+                $status = $throwable->getStatusCode();
+            }
+
+            return new JsonResponse([
+                'success' => false,
+                'error' => class_basename($throwable),
+                'message' => app()->hasDebugModeEnabled()
+                    ? $throwable->getMessage()
+                    : 'Server Error',
+            ], $status);
         });
     })
     ->create();
